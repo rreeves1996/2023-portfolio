f4a6f24bb23008dc6a2a1510d9e71ff345755ab0
Ryan Reeves <reeves0ryan@gmail.com>
2022-12-24 15:57:24 -0800

    Basic file structure, Roboto and Qualy, basic page sizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import './assets/style/style.css';
 
 function App() {
 	return (
