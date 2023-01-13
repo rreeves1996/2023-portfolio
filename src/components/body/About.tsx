57163df4dbfb4166cd93ba6eddc4ef0fff41e4d6
Ryan Reeves <reeves0ryan@gmail.com>
2023-01-12 21:18:43 -0800

    About page "slide" functionality

--- src/components/body/About.tsx
+++ src/components/body/About.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineDoubleRight } from 'react-icons/ai';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import portrait from '../../assets/images/me.png';
 import 'swiper/css';
 
+const SLIDES = [
+	<p>
+		<strong>
+			Born in 1996 in Seattle, Washington, my biggest interests in life have
+			always been in music and tech.{' '}
+		</strong>
+	</p>,
+	<p>
+		I am grateful to say that I've built my life in a way where I now have the
+		opportunity to be involved with both heavily day-to-day!
+	</p>,
+	<p>
+		Since graduating my Full-Stack Development boot camp on September 17th, I've
+		narrowed my focus into Front-End Development, primarily React.
+	</p>,
+];
+
 export default function About() {
+	const [currentSlide, setCurrentSlide] = useState<React.ReactElement[]>([
+		SLIDES[0],
+	]);
+
+	const handleAddSlide = () => {
+		setCurrentSlide((prevState) => [...prevState, SLIDES[currentSlide.length]]);
+	};
+
 	return (
 		<div className='about'>
 			<section className='title'>
@@ -21,29 +46,8 @@ export default function About() {
 				<div className='about-text'>
 					<h1>Learner, musician, engineer </h1>
 					<div className='about-body'>
-						<p>
-							<strong>
-								Born in 1996 in Seattle, Washington, my biggest interests in
-								life have always been in music and tech.{' '}
-							</strong>
-						</p>
-						<p>
-							I am grateful to say that I've built my life in a way where I now
-							have the opportunity to be involved with both, extensively -{' '}
-							<strong>everyday.</strong>
-						</p>
-						<p>
-							Though I excelled and enjoyed my time in masonry, my interest in
-							tech was piqued after hearing about how happy many tech workers
-							were. I was also curious because I had actually spent some time
-							coding in my youth already. I decided to register for a Full-Stack
-							Web Development bootcamp at the University of Washington, and
-							through the course I discovered that I'd like to begin a career in
-							Front End Development. Since graduating on September 17th, 2022,
-							I've been immersing myself in Front End Development, learning the
-							practices and technologies being used today to prepare myself for
-							my first position in Front End Development.
-						</p>
+						{currentSlide}
+						<button onClick={() => handleAddSlide()}>...</button>
 					</div>
 				</div>
 			</header>
