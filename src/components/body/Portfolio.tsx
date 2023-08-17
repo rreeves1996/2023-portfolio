bd35bdb5d67dfe76fe35611f84613e5e45e49262
Ryan Reeves <reeves0ryan@gmail.com>
2023-08-16 17:59:33 -0700

    Add surveyFriend to portfolio section

--- src/components/body/Portfolio.tsx
+++ src/components/body/Portfolio.tsx
@@ -5,7 +5,7 @@ import Slide from '../misc/Carousel/Slide';
 import TEAMEZ from '../../assets/images/teamez.png';
 import TODO_LIST from '../../assets/images/todo.png';
 import VIRTUAL_KEYBOARD from '../../assets/images/keyboard.png';
-import ARRAY_TOOL from '../../assets/images/arraytool.png';
+import SURVEYFRIEND from '../../assets/images/surveyfriend.png';
 import WEATHER_API from '../../assets/images/weather.png';
 import ScrollIntoView from 'react-scroll-into-view';
 
@@ -20,42 +20,6 @@ type Project = {
 };
 
 const PROJECTS: Project[] = [
-	{
-		deployment: 'https://array-tool.vercel.app/',
-		github: 'https://github.com/rreeves1996/array-tool',
-		img: ARRAY_TOOL,
-		title: 'Array Tool',
-		subtitle: 'React, TypeScript, SASS, Node',
-		desc: [
-			`This simple app is an array method visualizer, built with React (Vite) and SASS.`,
-			`I'd seen a similar app on LinkedIn and thought it would be interesting to reverse engineer, considering the icons persist whether they are in the array or not (using animations). I thought it would be fun to figure out how to make the icons persist while also keeping the code neat.`,
-			`It was an interesting problem to solve, because it wasn't as simple as performing the methods on an array, as then the icons would just disappear/reappear as opposed to persisting. I ended up solving the problem in a way that I thought remained true to the app while solving the issue: I had two "pseudo" arrays to represent the arrays being displayed to the user, while having the array with the actual icons in it as its own array. The icons were objects, and their positions were modified based on the two representational arrays. It was definitely fun to figure out!`,
-		],
-	},
-
-	{
-		deployment: 'https://teamez.herokuapp.com/',
-		github: 'https://github.com/rreeves1996/team-manager',
-		img: TEAMEZ,
-		title: 'TeamEZ',
-		subtitle: 'React, TypeScript, Redux, SASS, MySQL, Node, Express',
-		desc: [
-			`TeamEZ is a full-stack app that I created on my own following my boot camp graduation. I wanted to create a full-stack app from scratch that utilized the MERN stack, so I took inspiration from a command-line employee tracker I made in boot camp, expanded on it and its features, and created a full-stack version with my own front-end/UI. It allows the user easily keep track of employees and employee info and features a fully functional login/registration system created with JWT.`,
-			`Creating this app was a massive leap forward in my learning. Though the MERN stack is often-used, I used it with MySQL, and creating it from scratch proved to be difficult. However, having to look up and learn everything I needed for the app gave me a great understanding in how a lot of fundamenetal technologies and methods are used!`,
-		],
-	},
-	{
-		deployment: 'https://weather-gsepxpyo4-rreeves1996.vercel.app/',
-		github: 'https://github.com/rreeves1996/weather-api',
-		img: WEATHER_API,
-		title: 'Weather Dashboard',
-		subtitle: 'React, TypeScript, SASS, Node',
-		desc: [
-			`A simple weather app that I created on my own with React after my boot camp graduation.`,
-			`I created a weather app early on in my bootcamp with plain JavaScript, so I was interested to see how creating one on my own would go after learning a lot more, and learning React. I wanted a sleek UI that was easy to interact with but also nice to look at.`,
-			`I was able to learn a lot about keeping my code condensed and simple in this app, as most of the functionality could be accomplished with fewer lines of code, and it felt important to me to try and keep the complexity down. I am overall happy with what came of the app, and feel like I hit all of my goals I set out to accomplish before its creation!`,
-		],
-	},
 	{
 		deployment: 'https://rreeves-todo-list.herokuapp.com/',
 		github: 'https://github.com/rreeves1996/todo-list',
@@ -81,6 +45,44 @@ const PROJECTS: Project[] = [
 			`Coming out of boot camp, I was eager to try to tackle creating something entirely on my own, and I was eager to learn more about React. I ended up looking for simple app ideas to find one I could find and create that was easy/simple enough for me to create fresh out of boot camp, but hard enough to challenge me in certain aspects and give me some sort of growth, if not the experience creating the app itself.`,
 			`Four months after its creation, I decided to give the app a refactor. The code was a mess and, although it functioned properly, was not following commonpractice for the most part. I had also made my life so much more difficult than it needed to be in a lot of areas - much of my contrived, massive functions or workarounds were easily solved with few lines of code.`,
 			`I took full notes on the refactor and added them to the README - to read the full notes, click the 'Refactor' button! I'm very happy with how the cleanup turned out, and refactoring my old code was a valuable experience.`,
+		],
+	},
+	{
+		deployment: 'https://survey-app-silk.vercel.app/',
+		github: 'https://github.com/rreeves1996/survey-app',
+		img: SURVEYFRIEND,
+		title: 'surveyFriend',
+		subtitle:
+			'Next.js, React, TypeScript, Tailwind CSS, tRPC, Prisma, PostgreSQL',
+		desc: [
+			`My second full-stack project created on my own, this simple survey app, surveyFriend, allows users to login, create/manage surveys, take surveys, and view survey results in an intuitive, easy to navigate UI.`,
+			`This app was created using the T3 Stack (Next.js, Tailwind CSS, TypeScript, tRPC, Prisma) and utilizes PostgreSQL on a Supabase server for the backend. With this app, I was able to efficiently create a sleek and responsive UI using Tailwind CSS, which allowed me to focus most of my time on the actual funcionality of the app.`,
+			`surveyFriend uses OAuth 2 for login, allowing the user to sign in through GitHub, Discord, or Google providers. A unique (yes, this is a pun) problem I faced while making this app was signing in to different providers when accounts had the same email. I would attempt to login through GitHub, only to end up logged into my GitHub account.`,
+			`The problem had to do with unique identifiers. I realized that the Prisma default schema used the users' email as the unique identifier after clearing the users from my database and noting my GitHub login now functioned correctly. Had I known the email was the unique identifier on the User model, I would've changed it ages ago, as this can present many problems. I changed the unique identifier to the id, and all of the problems were solved. This taught me that I shouldn't put so much blind trust into the default generated schemas, ESPECIALLY when using Prisma with OAuth 2.`,
+		],
+	},
+
+	{
+		deployment: 'https://teamez.herokuapp.com/',
+		github: 'https://github.com/rreeves1996/team-manager',
+		img: TEAMEZ,
+		title: 'TeamEZ',
+		subtitle: 'React, TypeScript, Redux, SASS, MySQL, Node, Express',
+		desc: [
+			`TeamEZ is a full-stack app that I created on my own following my boot camp graduation. I wanted to create a full-stack app from scratch that utilized the MERN stack, so I took inspiration from a command-line employee tracker I made in boot camp, expanded on it and its features, and created a full-stack version with my own front-end/UI. It allows the user easily keep track of employees and employee info and features a fully functional login/registration system created with JWT.`,
+			`Creating this app was a massive leap forward in my learning. Though the MERN stack is often-used, I used it with MySQL, and creating it from scratch proved to be difficult. However, having to look up and learn everything I needed for the app gave me a great understanding in how a lot of fundamenetal technologies and methods are used!`,
+		],
+	},
+	{
+		deployment: 'https://weather-gsepxpyo4-rreeves1996.vercel.app/',
+		github: 'https://github.com/rreeves1996/weather-api',
+		img: WEATHER_API,
+		title: 'Weather Dashboard',
+		subtitle: 'React, TypeScript, SASS, Node',
+		desc: [
+			`A simple weather app that I created on my own with React after my boot camp graduation.`,
+			`I created a weather app early on in my bootcamp with plain JavaScript, so I was interested to see how creating one on my own would go after learning a lot more, and learning React. I wanted a sleek UI that was easy to interact with but also nice to look at.`,
+			`I was able to learn a lot about keeping my code condensed and simple in this app, as most of the functionality could be accomplished with fewer lines of code, and it felt important to me to try and keep the complexity down. I am overall happy with what came of the app, and feel like I hit all of my goals I set out to accomplish before its creation!`,
 		],
 	},
 ];
